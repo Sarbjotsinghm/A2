8802b3c5e87d7ce26a565f257c210a266b8b0741
Sarbjot Singh <sarbjots341@gmail.com>
2023-01-23 00:03:02 -0600

    added script file

---
+++ script.js
@@ -0,0 +1,11 @@
+function myFunction() {
+    var x = document.getElementById("mynavbar-links");
+    if (x.className === "navbar-links") {
+      x.className += " responsive";
+    } else {
+      x.className = "navbar-links";
+    }
+  }
+  function myFunction() {
+    document.getElementById("demo").innerHTML = "https://twitter.com/sarb_mann8";
+  }
